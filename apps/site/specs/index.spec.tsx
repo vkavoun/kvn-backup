1c03cd9dbbadd01f90d1b753b9d2d65e2802764b
Vladimir Kavoun <vkavoun@gmail.com>
2020-04-10 23:53:25 -0400

    was a horrible day of set ups

--- apps/site/specs/index.spec.tsx
+++ apps/site/specs/index.spec.tsx
@@ -5,12 +5,12 @@ import Index from '../pages/index';
 
 describe('Index', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(<Index userAgent={} />);
+    const { baseElement } = render(<Index userAgent={''} />);
     expect(baseElement).toBeTruthy();
   });
 
   it('should have a greeting as the title', () => {
-    const { getByText } = render(<Index userAgent={} />);
+    const { getByText } = render(<Index userAgent={''} />);
     expect(getByText('Welcome to site! [DEV]')).toBeTruthy();
   });
 });
